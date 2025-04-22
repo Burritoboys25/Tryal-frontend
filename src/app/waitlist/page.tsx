7d5826ca53cdec8e6877050716f1a80ad6528c7d
antho <anthonynuge0509@gmail.com>
2025-04-22 03:36:19 -0500

    Add base input and label component.

---
+++ src/app/waitlist/page.tsx
@@ -0,0 +1,19 @@
+import Container from '@/shared/components/layout/Container'
+
+const Waitlist = () => {
+  return (
+    <section className="-mt-[72px] flex min-h-screen items-center justify-center py-20">
+      <Container className="max-w-md text-left">
+        <h1 className="text-h2 mb-4">Get early access!</h1>
+        <p className="text-left">
+          We&apos;re building something exciting — the ultimate platform for discovering experiences
+          and activities. Be the first to know when we launch and get exclusive early updates.
+        </p>
+
+        <form></form>
+      </Container>
+    </section>
+  )
+}
+
+export default Waitlist
