40f8d6471f359dba3479529209ddb28191f6204f
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-04 17:21:29 -0700

    Remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           </div>
 
           <div className="relative z-0 flex w-full justify-center md:w-1/2">
-            {/* Glow Background bg-[#F4BCAB] bg-red-400*/}
+            {/* Glow Background*/}
             <div className="absolute top-1/2 left-1/2 z-[-1] h-[450px] w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#F4BCAB] blur-[120px] md:h-[600px] md:w-[600px]" />
 
             {/* Image Placeholder */}
