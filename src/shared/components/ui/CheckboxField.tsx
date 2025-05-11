681c5f07b0ba866b327c99d9fef10caf2e53dcfa
antho <anthonynuge0509@gmail.com>
2025-05-10 23:58:16 -0500

    Create checkbox field component.

---
+++ src/shared/components/ui/CheckboxField.tsx
@@ -0,0 +1,38 @@
+import { Checkbox } from './checkbox'
+import { Label } from './label'
+
+type CheckboxFieldProps = {
+  label: string
+  name: string
+  value: string
+  checked: boolean
+  onChange: (checked: boolean, value: string) => void
+  required?: boolean
+  disabled?: boolean
+}
+
+const CheckboxField = ({
+  label,
+  name,
+  value,
+  checked,
+  onChange,
+  required,
+  disabled,
+}: CheckboxFieldProps) => {
+  const id = `${name}-${value}`
+
+  return (
+    <div className="flex items-center gap-2">
+      <Checkbox
+        id={id}
+        checked={checked}
+        onCheckedChange={checked => onChange(!!checked, value)}
+        disabled={disabled}
+      />
+      <Label htmlFor={id}>{label}</Label>
+    </div>
+  )
+}
+
+export default CheckboxField
