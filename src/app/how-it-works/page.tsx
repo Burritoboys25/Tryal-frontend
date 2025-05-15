89df401259aa69fe6f37785eb84efa43a6842d80
antho <anthonynuge0509@gmail.com>
2025-05-15 18:30:09 -0500

    Style step element and make page responsive.

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -3,6 +3,7 @@ import DiscoverIcon from '@/shared/assets/icons/discover.svg'
 import BookIcon from '@/shared/assets/icons/book.svg'
 import ExploreIcon from '@/shared/assets/icons/explore.svg'
 import MainLayout from '@/shared/components/layout/MainLayout'
+import Container from '@/shared/components/layout/Container'
 
 const howItWorksSteps = [
   {
@@ -37,27 +38,31 @@ const howItWorksSteps = [
 const HowItWorksPage = () => {
   return (
     <MainLayout>
-      <section className="mx-auto mt-0 h-screen max-w-7xl px-4 sm:-mt-[72px] sm:px-0">
-        <div className="flex h-full flex-col items-center justify-center text-center">
-          <h2 className="text-h3 w-full py-10">How it works</h2>
-          {/* Card Container */}
-          <div className="grid max-w-7xl grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            {howItWorksSteps.map(({ step, title, description, icon: Icon }) => (
-              <div
-                key={step}
-                className="flex h-[276px] w-[272px] flex-col items-center justify-center rounded-2xl border-1 px-4 py-8"
-              >
-                <Icon />
-                <div className="space-y-4 text-center">
-                  <h3 className="text-sub1">{title}</h3>
-                  <p className="text-body4">{description}</p>
+      <Container>
+        <section className="flex min-h-[calc(100vh-600px)] items-center justify-center py-8 sm:py-12 lg:py-24">
+          <div className="flex h-full flex-col items-center justify-center text-center">
+            <h2 className="text-h3 mb-10 w-full">How it works</h2>
+            {/* Card Container */}
+            <div className="grid max-w-7xl grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4 lg:grid-cols-4">
+              {howItWorksSteps.map(({ step, title, description, icon: Icon }) => (
+                <div
+                  key={step}
+                  className="relative flex h-[276px] w-[272px] flex-col items-center justify-center rounded-2xl border-1 px-4 shadow-md"
+                >
+                  <Icon />
+                  <div className="mt-4 space-y-3 text-center">
+                    <h3 className="text-sub1">{title}</h3>
+                    <p className="text-body4">{description}</p>
+                  </div>
+                  <span className="bg-primary text-primary-foreground absolute -top-6 flex size-12 items-center justify-center rounded-full">
+                    {step}
+                  </span>
                 </div>
-                <span>{step}</span>
+              ))}
             </div>
-            ))}
           </div>
-        </div>
-      </section>
+        </section>
+      </Container>
     </MainLayout>
   )
 }
