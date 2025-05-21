9c8bd4893f438208a355400677a5163ece0427ed
Henry Nguyen <nguyenhenry682@gmail.com>
2025-05-21 11:18:36 -0500

    updated middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,19 @@ import { getToken } from 'next-auth/jwt'
 import { NextRequest } from 'next/server'
 
 export default async function middleware(req: NextRequest) {
+  if (process.env.NEXT_PUBLIC_ENV === 'dev') {
+    return NextResponse.next()
+  }
+
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
   if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url))
+    return NextResponse.redirect(new URL('/', req.url))
   }
 
   return NextResponse.next()
 }
 
-let matcher: string[]
-
-if (process.env.ENVIRONMENT === 'dev') {
-  matcher = []
-} else {
-  matcher = ['/explore', '/login', '/signup']
-}
-
 export const config = {
-  matcher
+  matcher: ['/explore/:path*', '/login', '/signup'],
 }
