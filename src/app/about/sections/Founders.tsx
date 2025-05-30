aca801397cae7fdc24ce22a7bc5b380f038caa31
Henry Le <henrycle16@gmail.com>
2025-05-29 20:50:57 -0500

    Headshots with email and linkedin updated

--- src/app/about/sections/Founders.tsx
+++ src/app/about/sections/Founders.tsx
@@ -1,22 +1,36 @@
 import Container from '@/shared/components/layout/Container'
-import { Mail, Phone } from 'lucide-react'
+import Image from 'next/image'
+import { Mail } from 'lucide-react'
+import Linkedin from '@/shared/assets/icons/linkedin.svg'
 
 const founders = [
   {
     name: 'Henry Le',
-    quote: '',
+    quote: 'Alone we can go fast but together we can go far.',
+    linkedin: 'https://www.linkedin.com/in/henry-le-585719187/',
+    avatar: '/ai_headshots/Henry_Le_AI_Pic.png',
+    email: 'Henrycle16@gmail.com',
   },
   {
     name: 'Henry Nguyen',
-    quote: '',
+    quote: 'Pressure is a privilege.',
+    linkedin: 'https://www.linkedin.com/in/henry-nguyen682/',
+    avatar: '/ai_headshots/Henry_N_AI_Pic.png',
+    email: 'nguyenhenry682@gmail.com',
   },
   {
     name: 'Calvin Nguyen',
-    quote: '',
+    quote: 'Rooted in trust. Growing through connection.',
+    linkedin: 'https://www.linkedin.com/in/calvintnguyen/',
+    avatar: '/ai_headshots/Calvin_AI_Pic.png',
+    email: 'cnguyenr42@gmail.com',
   },
   {
     name: 'Anthony Nguyen',
-    quote: '',
+    quote: 'Excellence isn’t a goal—it’s our baseline.',
+    linkedin: 'https://www.linkedin.com/in/anthony-nguyen-02861b331/',
+    avatar: '/ai_headshots/Anthony_AI_Pic.png',
+    email: 'anthonynuge0509@gmail.com',
   },
 ]
 
@@ -40,19 +54,29 @@ export default function Founders() {
                 key={founder.name}
                 className="flex flex-col items-center justify-between gap-3 rounded-xl border bg-white px-3 py-4 shadow-md"
               >
-                {/* Avatar Placeholder */}
-                <div className="h-24 w-24 rounded-full bg-gray-300" />
+                {/* Avatar */}
+                <Image
+                  src={founder.avatar}
+                  alt="Founder Avatar"
+                  width={100}
+                  height={100}
+                  className="mt-4 h-24 w-24 rounded-full object-cover object-[center_5%] shadow-lg md:h-32 md:w-32"
+                />
 
                 {/* Name + Quote */}
                 <div>
                   <h4 className="text-sub3">{founder.name}</h4>
-                  <p className="text-body2">&quot;{founder.quote}&quot;</p>
+                  <p className="text-body2 min-h-12 overflow-hidden">&quot;{founder.quote}&quot;</p>
                 </div>
 
                 {/* Lucide Icons */}
                 <div className="mt-2 flex gap-4">
-                  <Mail className="h-5 w-5" aria-label="Email" />
-                  <Phone className="h-5 w-5" aria-label="Phone" />
+                  <a href={`mailto:${founder.email}`}>
+                    <Mail className="h-6 w-6" aria-label="Email" />
+                  </a>
+                  <a href={founder.linkedin} target="_blank" rel="noopener noreferrer">
+                    <Linkedin className="h-6 w-6" aria-label="Linkedin" />
+                  </a>
                 </div>
               </div>
             ))}
