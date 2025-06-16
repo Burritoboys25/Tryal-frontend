b99cc91047ad20d7dd0c2e509a9f6318b318c7ee
Henry Nguyen <nguyenhenry682@gmail.com>
2025-06-15 22:37:59 -0500

    cleanup

--- src/modules/auth/types/next-auth.d.ts
+++ src/modules/auth/types/next-auth.d.ts
@@ -1,7 +1,5 @@
 import { DefaultSession } from 'next-auth'
 declare module 'next-auth' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-
   interface Session extends DefaultSession {
     user: {
       id: string
