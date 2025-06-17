a656b8a316c62edeb1edfd5fd0516f4184515759
Henry Le <henrycle16@gmail.com>
2025-06-16 22:37:41 -0500

    Bolding the header for TOS

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -9,15 +9,15 @@ const TermsPage = () => {
       <Container>
         <div className="space-y-8 md:space-y-12 2xl:space-y-24">
           <section className="pt-10">
-            <h1 className="text-center mb-16">Terms of Use</h1>
+            <h1 className="mb-16 text-center">Terms of Use</h1>
             <p>Last Updated: {termsUpdatedAt}</p>
-            <div className="pt-10">
-              <p>Welcome to Tryal!</p>
+            <div className="header1 pt-10">
+              <p className="text-h1">Welcome to Tryal!</p>
               <p className="pt-10">
-                Please read these Terms of Use (“Terms”) carefully before using our platform.
-                These Terms govern your access to and use of Tryal&apos;s website, mobile
-                application, and related services (collectively, the “Platform”). By accessing or
-                using the Platform, you agree to be bound by these Terms.
+                Please read these Terms of Use (“Terms”) carefully before using our platform. These
+                Terms govern your access to and use of Tryal&apos;s website, mobile application, and
+                related services (collectively, the “Platform”). By accessing or using the Platform,
+                you agree to be bound by these Terms.
               </p>
             </div>
           </section>
