7cc665361c09cebedfe7523fea24d8ba551e5cf5
antho <anthonynuge0509@gmail.com>
2025-06-20 17:16:44 -0500

    Helper function to build url params.

---
+++ src/modules/explore/libs/buildQueryParams.ts
@@ -0,0 +1,26 @@
+import { Filters } from '../types/filterTypes'
+
+export function buildQueryParams(filters: Filters): string {
+  const params = new URLSearchParams()
+
+  // If type is selected, add it to the query params
+  if (filters.type && filters.type.length > 0) {
+    params.append('categoryIds', filters.type.join(','))
+  }
+
+  // Convert frontend skillLevel to backend skillLevel
+  if (filters.skillLevel && filters.skillLevel.length > 0) {
+    params.append('skillLevel', filters.skillLevel.join(','))
+  }
+
+  // Convert frontend duration to backend duration
+  if (
+    filters.duration !== undefined &&
+    filters.duration !== null &&
+    filters.duration !== Infinity
+  ) {
+    params.append('duration', String(filters.duration))
+  }
+
+  return params.toString()
+}
