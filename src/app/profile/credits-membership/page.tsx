2fa03e90c89ccd004ff667de4f3ce8accd52186f
Calvin Nguyen <cnguyenr42@gmail.com>
2025-07-03 13:57:30 -0700

    Refactor credits page

--- src/app/profile/credits-membership/page.tsx
+++ src/app/profile/credits-membership/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Button } from '@/shared/components/ui/base/button'
-import CreditIcon from '@/shared/assets/icons/credit.svg'
-import { CreditCard } from 'lucide-react'
+import CreditsPage from '@/modules/profile/components/CreditsPage'
 
 const page = () => {
   return (
@@ -11,40 +9,7 @@ const page = () => {
         <p className="text-body2">Manage your credits and payment details.</p>
       </div>
 
-      <div className="">
-        <h2 className="text-sub1">Credits balance</h2>
-        <div className="mt-2 flex w-fit items-center gap-2 rounded-[.5rem] bg-[#FADDD5] px-4 py-2">
-          <CreditIcon className="h-6 w-6" aria-hidden="true" />{' '}
-          <p className="text-sub2">30</p>
-        </div>
-      </div>
-
-      <div>
-        <h2 className="text-sub1">Current plan</h2>
-        <p className="text-body2 mt-1">Starter plan</p>
-        <Button type="button" variant="outline" className="mt-2 border-2 px-6 cursor-pointer hover:bg-[#FADDD5]">
-          <p className="text-button">Change plan</p>
-        </Button>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <h2 className="text-sub1">Payment info</h2>
-        <p className="text-body2">May 15, 2025</p>
-        <div className="flex items-center gap-3">
-          <CreditCard />
-          <div> **** **** **** </div>
-          <p className="text-body1 font-semibold">0024</p>
-        </div>
-
-        <div className="flex gap-2">
-          <Button type="button" variant="outline" className="border-2 px-6 cursor-pointer hover:bg-[#FADDD5]">
-            <p className="text-button">Update Payment</p>
-          </Button>
-          <Button type="button" variant="text" className="px-6 cursor-pointer hover:bg-[#FADDD5]">
-            <p className="text-button text-[#FA1D1D]">Cancel membership</p>
-          </Button>
-        </div>
-      </div>
+      <CreditsPage />
     </div>
   )
 }
