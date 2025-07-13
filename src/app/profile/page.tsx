36a185513469008f2dbcaaa54ad1d29abd4a64b8
Calvin Nguyen <cnguyenr42@gmail.com>
2025-07-13 15:46:34 -0700

    Remove unecessary comments

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
 import ProfilePage from '@/modules/profile/components/ProfilePage'
 
-// const userDTO = {
-//   userId: '96204bc2-ddf9-4708-b841-ead5a153b308',
-//   firstName: 'Katherine',
-//   lastName: 'Payton',
-//   email: 'kpayton@gmail.com',
-//   phoneNumber: '2226595555',
-//   dateOfBirth: '2000-01-01',
-//   gender: 'female',
-//   profileImageUrl: '',
-//   creditBalance: 0,
-//   stripeCustomerId: '',
-// }
-
 const Profile = async () => {
-  // get userid from session -- currently hardcoded
+  // TODO: get userid from session -- currently hardcoded
   const userId = '96204bc2-ddf9-4708-b841-ead5a153b308'
   const data = await fetch(`${process.env.BACKEND_URL}/api/users/${userId}`)
   const profileData = await data.json()
@@ -28,7 +15,6 @@ const Profile = async () => {
       <p className="text-body2">
         Your information here will be shared with the business when you make a booking.
       </p>
-      {/* {...profileData.userDTO} {...userDTO} */}
       <ProfilePage {...profileData.userDTO} />
     </div>
   )
