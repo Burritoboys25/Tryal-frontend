bede2e6c6c13a9e3e73aaa5315bb78c6dc877dba
Calvin Nguyen <cnguyenr42@gmail.com>
2025-08-01 16:39:56 -0700

    Fix build error

--- src/app/api/profile/[id]/bookings/route.ts
+++ src/app/api/profile/[id]/bookings/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(_: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
-
-  console.log('User Id: ' + id);
 
   try {
     const result = await fetch(`${process.env.BACKEND_URL}/api/bookings/user/${id}`, {
