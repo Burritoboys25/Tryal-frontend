d763312dfe91d4c57a4f2cb92258ebfdc833719c
Calvin Nguyen <cnguyenr42@gmail.com>
2025-08-01 21:21:45 -0700

    Fix run build errors

--- src/app/profile/experiences/page.tsx
+++ src/app/profile/experiences/page.tsx
@@ -2,21 +2,29 @@ import React from 'react'
 import ExperiencePage from '@/modules/profile/components/experiences/ExperiencePage'
 
 const Page = async () => {
-  // TODO: get userid from session -- currently hardcoded
-  const userId = '272d2788-ee1e-4056-ae09-4829aff17909'
-  const data = await fetch(`${process.env.NEXTAUTH_URL}/api/profile/${userId}/bookings`)
-  const bookingsData = await data.json()
-  const bookmarksData = await fetch(`${process.env.NEXTAUTH_URL}/api/users/${userId}/bookmarks`)
-  const bookmarks = await bookmarksData.json();
+  //   // TODO: get userid from session -- currently hardcoded
+  const userId = '272d2788-ee1e-4056-ae09-4829aff17909';
+
+  const baseUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
+
+  const bookingsRes = await fetch(`${baseUrl}/api/profile/${userId}/bookings`, {
+    cache: 'no-store', // disables static caching
+  });
+  const bookings = await bookingsRes.json();
+
+  const bookmarksRes = await fetch(`${baseUrl}/api/users/${userId}/bookmarks`, {
+    cache: 'no-store',
+  });
+  const bookmarks = await bookmarksRes.json();
 
   return (
     <div className="">
       <h1 className="text-h3">Experiences</h1>
       <p className="text-body2 mt-1">View and/or manage your experiences.</p>
 
-      <ExperiencePage bookings={bookingsData} bookmarks={bookmarks} />
+      <ExperiencePage bookings={bookings} bookmarks={bookmarks} />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
