792011b8162acdb2d3ffae430cb57fffdfc23542
antho <anthonynuge0509@gmail.com>
2025-09-19 18:17:29 -0500

    Apply teal class to layout to not mess with other non landing pages for now.,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${manrope.variable} ${rubik.variable} `}>
+    <html lang="en" className={`${manrope.variable} ${rubik.variable} teal`}>
       <body className="w-full">
         <NextAuthSessionProvider>
           <UserProvider>
