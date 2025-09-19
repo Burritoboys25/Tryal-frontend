70ef0a122a5e5e12f3578ace8737487c6b7faa9a
Henry Nguyen <nguyenhenry682@gmail.com>
2025-09-19 13:06:54 -0500

    added activeSubscription type to user

--- src/shared/types/userTypes.ts
+++ src/shared/types/userTypes.ts
@@ -4,8 +4,19 @@ export interface User {
   firstName: string
   lastName: string
   remember?: boolean
+  activeSubscription?: UserSubscription
 }
 export interface UserBookmarks {
   userId: string
   businessIds: string[]
+}
+
+export interface UserSubscription {
+  subscriptionId: string
+  userId: string
+  planId: string
+  subscriptionStatus: string
+  autoRenew: boolean
+  startAt: string
+  endAt?: string
 }
