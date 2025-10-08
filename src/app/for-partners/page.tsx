f45eb6ef6fc8cef5452199783134f6a24a3308a7
Calvin Nguyen <cnguyenr42@gmail.com>
2025-10-07 19:45:59 -0700

    Add navbar to for partners and about page

--- src/app/for-partners/page.tsx
+++ src/app/for-partners/page.tsx
@@ -10,6 +10,7 @@ import FaFeatures from '../_sections/forPartners/FaFeatures'
 import LenisProvider from '@/shared/lib/LenisProvider'
 import PathScrollSteps from '../_sections/forPartners/PathScrollSteps'
 import MainFooter from '@/shared/components/layout/MainFooter'
+import LandingNavbar from '@/shared/components/layout/LandingNavbar'
 
 gsap.registerPlugin(useGSAP)
 
@@ -17,6 +18,7 @@ const ForPartnersPage = () => {
   return (
     <>
       <LenisProvider>
+        <LandingNavbar />
         <div className="space-y-4 md:space-y-8">
           <FPHero />
           <Benefits />
